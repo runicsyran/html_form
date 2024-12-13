0d856927cf1ed5733ef61567f8a0697f3bed2a67
Michelangelo Cuccui <michelangelo.cuccui@ptpvenezia.edu.it>
2024-12-13 11:41:38 +0100

    aggiornamento 2024 12 13 11:41

--- bash/onlyFtpOfLastCmt.sh
+++ bash/onlyFtpOfLastCmt.sh
@@ -13,8 +13,32 @@ function uploadFilesFromCommit() {
         # Esegui il comando curl per caricare il file
         local curlCommand="curl -T \"$file\" \"$ftpRequest\" --ftp-pasv --ftp-create-dirs"
         echo -e "$curlCommand"
-        eval "$curlCommand"
-        echo "$relativePath/$fileName caricato con successo."
+        
+        # Esegui curl e cattura l'output e il codice di uscita
+        local curlOutput
+        curlOutput=$(eval "$curlCommand" 2>&1)
+        local curlExitCode=$?
+
+        # Controlla se curl ha restituito un errore
+        if [ $curlExitCode -ne 0 ]; then
+            # Se l'errore è "Failed to open/read local data", rimuovi il file dal server
+            if [[ "$curlOutput" == *"Failed to open/read local data"* ]]; then
+                echo "Errore durante il caricamento di $file. Rimozione dal server in corso..."
+                
+                # Comando per eliminare il file dal server FTP
+                local deleteCommand="curl -Q \"DELE $relativePath/$fileName\" \"ftp://michelangelocuccui:DioBrando21@ftp.michelangelocuccui.altervista.org:21/\" --ftp-pasv"
+                echo -e "$deleteCommand"
+                eval "$deleteCommand"
+                
+                echo "File $relativePath/$fileName rimosso dal server."
+            else
+                # Per altri tipi di errori, mostra l'output di curl
+                echo "Errore durante il caricamento di $file:"
+                echo "$curlOutput"
+            fi
+        else
+            echo "$relativePath/$fileName caricato con successo."
+        fi
     done
 }
 
