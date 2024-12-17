5be5f4f20a2792633b2d69567c08b5731e8483be
Michelangelo Cuccui <michelangelo.cuccui@ptpvenezia.edu.it>
2024-12-17 10:27:41 +0100

    aggiornamento 2024 12 17 10:27

--- html_form/index.php
+++ html_form/index.php
@@ -89,7 +89,7 @@
             <div id="gender-error" class="error"></div>
 
             <label for="data">Data:</label><br>
-            <span id="dataPickerSlot"></span>
+            <input type="text" name="data" id="dataPicker">
             <div id="data-error" class="error"></div>
 
             <label for="city">Città:</label><br>
@@ -112,9 +112,9 @@
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
         <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
         <script /*src="./dataPicker.js"*/>
-        $(function DataPicker(params) {
-            
-        }
+        $(function() {
+            $("#dataPicker").datePicker()
+        });
         </script>
 
     </body>
