8032484e132d9d8d7c271aa429ffad2c69157e21
Michelangelo Cuccui <michelangelo.cuccui@ptpvenezia.edu.it>
2025-01-27 12:37:19 +0100

    aggiornamento 2025 01 27 12:37

--- Sessioni/Sessioni.php
+++ Sessioni/Sessioni.php
@@ -18,5 +18,31 @@ $_SESSION['cognome'] = 'Cuccui';
 echo 'Variabili di sessione settate.<br>';
 ?>
 
+
+<?php
+//Visualizzare le variabili di sessione
+echo 'Variabili di sessione:<br>';
+echo $_SESSION['nome'].'<br>';
+echo $_SESSION['cognome'].'<br>';
+?>
+
+<?php
+//Eliminare una variabile di sessione
+unset($_SESSION['nome']);
+echo 'Variabile di sessione eliminata.<br>';
+?>
+
+
+<?php
+//Eliminare tutte le variabili di sessione
+session_unset();
+echo 'Variabili di sessione eliminate.<br>';
+?>
+
+<?php
+//Distruggere la sessione
+session_destroy();
+echo 'Sessione distrutta.<br>';
+?>
 </body>
 </html>
