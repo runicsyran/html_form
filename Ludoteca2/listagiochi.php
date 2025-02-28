0920abc1969fd7ac4dd677a8d15a95b6e3b3cf0b
Michelangelo Cuccui <mickeycp2006@gmail.com>
2025-02-28 10:47:54 +0100

    aggiornamento 2025 02 28 10:47

--- Ludoteca2/listagiochi.php
+++ Ludoteca2/listagiochi.php
@@ -25,6 +25,7 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Giochi Disponibili in Ludoteca</title>
     <script src="js/scripts.js"></script>
+    <script src="js/Popup.js"></script>
 </head>
 <body>
     <h1>Elenco Giochi:</h1>
